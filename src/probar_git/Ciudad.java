04b7610e3b0262e19b7c76b07f19ba505705ef1e
Alum <contact@example.com>
2022-05-06 17:14:32 +0200

    Primer commit

---
+++ src/probar_git/Ciudad.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package probar_git;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ciudad {
+    private String nombre;
+            private int poblacion;
+            public Ciudad(String nombre, int poblacion){
+                this.nombre=nombre;
+                this.poblacion=poblacion;
+            }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPoblacion() {
+        return poblacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPoblacion(int poblacion) {
+        this.poblacion = poblacion;
+    }
+
+        
+}
